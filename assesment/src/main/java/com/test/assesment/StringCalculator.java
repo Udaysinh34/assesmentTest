a1e0a7f9aa3aeeb5e5cd00dffa77d84d182cfe28
Udaysinh patil <uday.patil@globtechnoitsolution.com>
2024-10-26 12:43:41 +0530

    here actually we see test cases actually passing so move forward with small changes

--- assesment/src/main/java/com/test/assesment/StringCalculator.java
+++ assesment/src/main/java/com/test/assesment/StringCalculator.java
@@ -1,8 +1,25 @@
 package com.test.assesment;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class StringCalculator {
 
     int add(String numbers){
-        return 0;
+        int stringLength = numbers.length();
+        int addition = 0;
+        String numString = new String();
+        for(int i=0;i<stringLength;i++){
+
+
+            if(numbers.charAt(i) != ','){
+                numString = numString + (String.valueOf(numbers.charAt(i)));
+            } else {
+                addition = Integer.parseInt(addition + numString);
+                numString = new String();
+            }
+        }
+        addition = addition + Integer.parseInt(numString);
+        return addition;
     }
 }
